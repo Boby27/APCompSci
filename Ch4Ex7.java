bcbd3763bb02645b2ce3bd7f8c803b25ab5e4fd1
Marodox <cjzeiger@gmail.com>
2013-11-17 16:42:42 -0500

    Ch4Ex7

---
+++ Ch4Ex7.java
@@ -0,0 +1,24 @@
+public class Ch4Ex7{
+
+     public static void main(String []args){
+         int input = 1;
+         boolean answer = false;
+         int place = 1;
+         while((input-squares(place) >= 0)){
+             if (input-squares(place) == 0){
+                answer = true;
+             }
+             place++;
+         }  
+         System.out.println(answer);
+     }
+     public static int squares(int p){
+         int sum = 0;
+         for(int j = 1;j<=p;j++){
+            sum = sum + ((2*j) - 1);
+        }
+        return sum;
+     }
+     
+     
+}
