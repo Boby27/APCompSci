fb83202bd4f6258f2f34948dee4a3e0ba0c92ec5
marodox <cjzeiger@gmail.com>
2014-02-03 21:25:26 -0500

    idk

---
+++ src/com/compsci/chapter6/Ex10.java
@@ -0,0 +1,9 @@
+package com.compsci.chapter6;
+
+public class Ex10 {
+	public static void main(String[] args){
+		double x = -2.5;
+		long round = Math.round(x);
+		System.out.println(round);
+	}
+}
