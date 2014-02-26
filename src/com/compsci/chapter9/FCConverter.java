c02c817a366e6f88fd889d834e93ce8f54128701
marodox <cjzeiger@gmail.com>
2014-02-26 12:28:53 -0500

    chapter 9

---
+++ src/com/compsci/chapter9/FCConverter.java
@@ -0,0 +1,28 @@
+package com.compsci.chapter9;
+
+public class FCConverter
+{
+  private double mFahrenheit;
+  private double mCelsius;
+  
+  public void setFahrenheit(double degrees)
+  {
+    mFahrenheit = degrees;
+  }
+
+  public double getCelsius()
+  {
+    return 0;
+  }
+
+  public void setCelsius(double degrees)
+  {
+    mCelsius = degrees;
+  }
+
+  public double getFahrenheit()
+  {
+    return 0;
+  }
+
+}
